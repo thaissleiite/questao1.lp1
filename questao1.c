9da32509ce87cc82971c81751eec0a4e0dcd001e
Thais Leite <145159951+thaissleiite@users.noreply.github.com>
2023-09-26 15:25:29 -0300

    Add files via upload

---
+++ questao1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+     int main (){
+
+     int x, y;
+
+     printf("insira o numero x:\n");
+     scanf("%d", &x);
+     printf("insira o numero y:\n");
+     scanf("%d", &y);
+
+       int soma = x + y;
+       int produto = x * y;
+       int diferenca = x - y;
+       int quociente = x / y;
+       int resto = x % y;
+
+     printf("a soma de %d eh:\n");
+     printf("o produto de %d eh:\n");
+     printf("a diferenca de %d eh:\n");
+     printf("o resto de %d eh:\n");
+
+
+ }
