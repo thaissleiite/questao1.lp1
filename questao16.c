337b5b9ea7dcb240c9e79cf5c792ba545c0ac5c4
Thais Leite <145159951+thaissleiite@users.noreply.github.com>
2023-10-02 01:44:47 -0300

    Update questao16.c

--- questao16.c
+++ questao16.c
@@ -1,25 +1,24 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 int main () {
-float a,b,c;
+   float a,b,c;
 
-printf("Insira um numero para a:\n");
-scanf("%f",&a);
+      printf("Insira um numero para a:\n");
+      scanf("%f",&a);
 
-printf("Insira um numero para b:\n");
-scanf("%f",& b);
+      printf("Insira um numero para b:\n");
+      scanf("%f",& b);
 
-printf("Insira um numero para c:\n");
-scanf("%f", &c);
+      printf("Insira um numero para c:\n");
+      scanf("%f", &c);
 
-if (a > 0 && b > 0 && c > 0) {
-        if (a < (b + c) && b < (a + c) && c < (a + b))
+          if (a > 0 && b > 0 && c > 0) {
+          if (a < (b + c) && b < (a + c) && c < (a + b))
             printf("a, b, c podem ser lados de um triangulo.");
-        else
-            printf("a, b, c não podem ser lados de um triangulo.");
-    } else {
-        printf("a, b, c precisam ser maiores que 0.");
+          else
+            printf("a, b, c nÃ£o podem ser lados de um triangulo.");
+          } else {
+            printf("a, b, c precisam ser maiores que 0.");
     }
 
 return 0;
