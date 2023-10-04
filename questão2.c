8615563f914d0f0e5b02779414f4b9aae10e93df
Thais Leite <145159951+thaissleiite@users.noreply.github.com>
2023-10-03 22:19:37 -0300

    Create questão2.c

---
+++ questão2.c
@@ -0,0 +1,26 @@
+//programa em c que o usuário fornece dois números inteiros, imprime (eh maior caso entre os dois números foi obtido o maior) ou imprima(estes numeros sao iguais - caso os dois numeros inteiros obtidos tenham sido iguais) 
+
+#include <stdio.h> //biblioteca
+  int main (){ //funcao principal
+    int m, n; //tipo de dado + variavel
+
+      printf("Insira o primeiro numero inteiro:\n");
+        scanf("%d", m);
+      printf("Insira o primeiro numero inteiro:\n");
+        scanf("%d", n);
+
+          if(m > n){
+             printf("%d eh maior que %d\n", m);
+          }
+
+          if(m == n){
+            printf("%d eh igual a %d.\n", n);
+
+          if(n > m){
+            printf("%d eh maior que %d\n", m);
+
+          }
+            return 0;
+          
+    
+  }
