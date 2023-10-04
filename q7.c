bdffba96462bfa5ab6d292ef03d33bdf4edf0ff6
Thais Leite <145159951+thaissleiite@users.noreply.github.com>
2023-10-04 10:13:03 -0300

    Create q7.c

---
+++ q7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+
+    printf(" T T T T T T T\n");
+    printf("           T \n");
+    printf(".          T \n");
+    printf("           T\n");
+    printf("           T\n");
+    printf("           T \n");
+   
+
+    printf("   S S S \n");
+    printf(" S \n");
+    printf("S \n");
+    printf(" S \n");
+    printf("   S S S\n");
+    printf("         S \n");
+    printf("          S \n");
+    printf("         S \n");
+    printf("   S S S \n\n");
+
+    printf("L\n");
+    printf("L\n");
+    printf("L\n");
+    printf("L\n");
+    printf("L\n");
+    printf("L\n");
+    printf("L\n");
+    printf("L\n");
+     printf("L L L L L \n");
+
+}
