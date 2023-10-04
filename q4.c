4a1152e8249e9dd9c6c6bc832419b79ea3757903
Thais Leite <145159951+thaissleiite@users.noreply.github.com>
2023-10-04 00:08:08 -0300

    Create q4.c

---
+++ q4.c
@@ -0,0 +1,14 @@
+//programa em C que ler o raio de um circulo, imprime o diametro e o valor da circuferencia e da area
+
+#include <stdio.h>
+  int main(){
+    float r;
+      printf("insira o valor do raio do circulo:\n"); //raio do circulo é a metade do comprimento do diamentro
+       scanf("%f", r);
+
+    float diametro = r * 2;
+    float circuferencia = pi = 3,14 * 2 * r;
+    float area = pi = 3,13, * r * r;
+
+      printf("O diametro eh %f e %f.\n, diamentro
+  }
